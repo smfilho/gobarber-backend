8c2c93aea69e30d4c9a58f0e3af94e5cd827f7fe
Silvio Melo Filho <smfilho@gmail.com>
2020-05-12 19:49:37 -0500

    Avatar uploads done

---
+++ src/config/upload.ts
@@ -0,0 +1,18 @@
+import path from 'path'; // fazer funcionar os paths em qq OS
+import crypto from 'crypto'; // para usar fileHash, certificar q o nome do arq. a ser uploaded é único
+import multer from 'multer';
+
+const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
+
+export default {
+  directory: tmpFolder,
+  storage: multer.diskStorage({
+    destination: tmpFolder,
+    filename(request, file, callback) {
+      const fileHash = crypto.randomBytes(10).toString('HEX');
+      const fileName = `${fileHash}-${file.originalname}`;
+
+      return callback(null, fileName);
+    },
+  }),
+};
